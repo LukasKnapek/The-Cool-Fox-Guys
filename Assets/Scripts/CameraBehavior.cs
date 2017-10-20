e20f8e1cdd248e6343a97e43346b7c9a7c6bd9cd
Lukas Knapek <knapek.luk@googlemail.com>
2017-10-20 23:45:33 +0100

    Dynamic camera movement

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    private Transform playerTransform;
+    private float yOffset = 2.5f;
+
+    // Use this for initialization
+
+    public void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        Vector3 cameraPos = new Vector3(playerTransform.position.x, playerTransform.position.y+yOffset, transform.position.z);
+        transform.position = cameraPos;
+
+    }
+}
