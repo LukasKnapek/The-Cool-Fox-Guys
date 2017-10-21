202e389bbcd2036886ce3d639a8e1fe87ea89558
Lukas Knapek <knapek.luk@googlemail.com>
2017-10-21 22:02:33 +0100

    Working two player camera

---
+++ Assets/Scripts/Camera/CameraCheck.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCheck : MonoBehaviour {
+
+    private Camera mainCamera;
+ 
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.Find("MainCamera").GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(this.GetComponent<SpriteRenderer>().isVisible);
+	}
+
+    public void OnBecameInvisible()
+    {
+        //mainCamera.GetComponent<CameraBehavior>().IncreaseCameraSize();
+    }
+}
